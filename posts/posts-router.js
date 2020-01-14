48194c35d1f178fc098fa5d5a44e31052a21d765
dueka <ekanemd@live.com>
2020-01-15 00:41:20 +0100

    created the post request

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -2,7 +2,7 @@ const express = require("express");
 const Posts = require("../data/db");
 const router = express.Router();
 
-router.get("/api/posts", (req, res) => {
+router.get("/", (req, res) => {
   Posts.find(req.query)
     .then(posts => {
       res.status(200).json(posts);
@@ -11,6 +11,30 @@ router.get("/api/posts", (req, res) => {
       console.log(error);
       res.status(500).json({
         errorMessage: "The posts information could not be retrieved."
+      });
+    });
+});
+
+router.post("/", (req, res) => {
+  const newPost = {
+    title: req.body.title,
+    contents: req.body.contents
+  };
+
+  if (!newPost.title || !newPost.contents) {
+    res.status(400).json({
+      errorMessage: "Please provide title and contents for the post."
+    });
+  }
+
+  insert(newPost)
+    .then(post => {
+      res.status(201).json(post);
+      post;
+    })
+    .catch(error => {
+      res.status(500).json({
+        error: "There was an error while saving the post to the database"
       });
     });
 });

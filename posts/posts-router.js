338c7dc88a3977cc9e527441e237b341d8409adc
dueka <ekanemd@live.com>
2020-01-15 08:24:24 +0100

    created the remaining routes

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -65,3 +65,93 @@ router.post("/:id/comments", (req, res) => {
       });
     });
 });
+
+router.get("/:id", (req, res) => {
+  const { id } = req.params;
+  findById(id)
+    .then(post => {
+      if (post.length > 0) {
+        res.status(200).json(post);
+      } else {
+        res.status(404).json({
+          message: "The post with the specified ID does not exist."
+        });
+      }
+    })
+    .catch(error => {
+      res.status(500).json({
+        error: "The post information could not be retrieved."
+      });
+    });
+});
+
+router.get("/:id/comments", (req, res) => {
+  const { id } = req.params;
+  findPostComments(id)
+    .then(comments => {
+      if (comments) {
+        res.status(200).json(comments);
+      } else {
+        res.status(404).json({
+          message: "The post with the specified ID does not exist."
+        });
+      }
+    })
+    .catch(error => {
+      res.status(500).json({
+        error: "The comments information could not be retrieved."
+      });
+    });
+});
+
+router.delete("/:id", (req, res) => {
+  const { id } = req.params;
+  remove(id)
+    .then(deleted => {
+      if (deleted) {
+        res.status(202).json(`Post got deleted`);
+        deleted;
+      } else {
+        res.status(404).json({
+          message: "The post with the specified ID does not exist."
+        });
+      }
+    })
+    .catch(error => {
+      res.status(500).json({
+        error: "The post could not be removed"
+      });
+    });
+});
+
+router.put("/:id", (req, res) => {
+  const { id } = req.params;
+  const post = req.body;
+
+  if (!post.title || !post.contents) {
+    res.status(400).json({
+      errorMessage: "Please provide title and contents for the post."
+    });
+  } else {
+    update(id, post)
+      .then(updated => {
+        if (updated) {
+          res.status(200).json({
+            message: "post updated",
+            updated
+          });
+        } else {
+          res.status(404).json({
+            message: "The post with the specified ID does not exist."
+          });
+        }
+      })
+      .catch(() => {
+        res
+          .status(500)
+          .json({ error: "The post information could not be modified." });
+      });
+  }
+});
+
+module.exports = router;
